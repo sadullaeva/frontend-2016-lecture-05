dda3309733710f5d632281457b8c6982dba54b4a
Sadullaeva Marina <sadullaeva.marina@gmail.com>
2016-12-16 10:35:56 +0600

    Fixed rendering

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,18 +3,16 @@ $(document).ready(function () {
   nunjucks.configure({
     autoescape: true,
     web: {
-      async: true
+      async: false
     }
   });
 
   $.ajax({
     url: "./mockapi/getAllArticles.json",
     success: function(data, status) {
-      for (item in data.articles) {
-        nunjucks.render('./partials/article.html', data.articles[item], function (err, res) {
-          $('.js-articles').append(res);
-        });
-      }
+      nunjucks.render('./partials/article.html', data, function (err, res) {
+        $('.js-articles').append(res);
+      });
     }
   });
 
